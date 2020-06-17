54fa1f40def1e8230918198744876211a02a61b4
salomonv <salomon@salomonv.nl>
2020-06-17 12:34:21 +0200

    - AircraftInFlow can now be updated by the flow

--- conflicts/simulate.py
+++ conflicts/simulate.py
@@ -1,29 +1,37 @@
+import itertools
 import logging
 
 import numpy as np
-
 from pint import UnitRegistry, Quantity
+
 ureg = UnitRegistry()
 
 
 class Aircraft:
-    horizontal_separation_requirement = 3       # nm
-    vertical_separation_requirement = 1000      # ft
+    horizontal_separation_requirement = 3  # nm
+    vertical_separation_requirement = 1000  # ft
     dtype = np.float
+    vx, vy, v = None, None, None
 
     def __init__(self, position, trk, gs, alt, vs, callsign, active=True):
-        self.position = np.array(position, dtype=self.dtype)   # nm
-        self._gs = gs                                           # knots
-        self.trk = trk                                          # degrees
-        self.alt = alt                                          # feet
-        self.vs = vs                                            # feet per minute
+        if isinstance(position, np.ndarray):
+            self.position = position
+        else:
+            self.position = np.array(position, dtype=self.dtype)  # nm
+        self._gs = gs  # knots
+        self._trk = trk  # degrees
+        self._trk_rad = np.radians(trk)
+        self.alt = alt  # feet
+        self._vs_fpm = vs  # feet per minute
+        self.vs_fph = vs * 60.
         self.callsign = callsign
         self.active = active
+        self.calculate_speed_vector()
 
-    def _update_speed_or_track(self):
-        self.vx = self._gs * np.sin(self._trk_rad)              # knots
-        self.vy = self._gs * np.cos(self._trk_rad)              # knots
-        self.v = np.array([self.vx, self.vy])                   # knots
+    def calculate_speed_vector(self):
+        self.vx = self._gs * np.sin(self._trk_rad)  # knots
+        self.vy = self._gs * np.cos(self._trk_rad)  # knots
+        self.v = np.array([self.vx, self.vy])  # knots
 
     @property
     def gs(self):
@@ -31,8 +39,7 @@ class Aircraft:
 
     @gs.setter
     def gs(self, gs):
-        self._gs = gs
-        self._update_speed_or_track()
+        raise NotImplementedError()
 
     @property
     def trk(self):
@@ -40,9 +47,7 @@ class Aircraft:
 
     @trk.setter
     def trk(self, trk):
-        self._trk = trk
-        self._trk_rad = np.radians(trk)
-        self._update_speed_or_track()
+        raise NotImplementedError()
 
     @property
     def vs(self):
@@ -50,8 +55,7 @@ class Aircraft:
 
     @vs.setter
     def vs(self, vs):
-        self._vs_fpm = vs
-        self.vs_fph = vs * 60.
+        raise NotImplementedError()
 
     def step(self, dt):
         if isinstance(dt, Quantity):
@@ -64,21 +68,67 @@ class Aircraft:
     def __and__(self, other):
         if not isinstance(other, type(self)):
             raise NotImplementedError()
-        return np.linalg.norm(self.position - other.position) <= self.horizontal_separation_requirement and\
+        return np.linalg.norm(self.position - other.position) <= self.horizontal_separation_requirement and \
                np.abs(self.alt - other.alt) <= self.vertical_separation_requirement
 
 
+class SingleAircraft(Aircraft):
+    @Aircraft.gs.setter
+    def gs(self, gs):
+        self._gs = gs
+        self.calculate_speed_vector()
+
+    @Aircraft.trk.setter
+    def trk(self, trk):
+        self._trk = trk
+        self._trk_rad = np.radians(trk)
+        self.calculate_speed_vector()
+
+    @Aircraft.vs.setter
+    def vs(self, vs):
+        self._vs_fpm = vs
+        self.vs_fph = vs * 60.
+
+
+class AircraftInFlow(Aircraft):
+    def __init__(self, index, position, trk, gs, alt, vs, callsign, active):
+        self.index = index
+        super().__init__(position[index:index + 1], trk[index:index + 1], gs[index:index + 1], alt[index:index + 1],
+                         vs[index:index + 1], callsign[index:index + 1], active[index:index + 1])
+
+    def step(self, *args, **kwargs):
+        raise RuntimeError("Step is not implemented for AircraftInFlow")
+
+
 class Flow:
-    def __init__(self, aircraft_list):
-        if not np.all(map(lambda ac: isinstance(ac, Aircraft), aircraft_list)):
-            raise ValueError()
-        self.callsign_map = {aircraft.callsign: i for i, aircraft in enumerate(aircraft_list)}
-        dtype = Aircraft.dtype
-        self.position = np.array([aircraft.position for aircraft in aircraft_list], dtype=dtype)
-        self.v = np.array([aircraft.v for aircraft in aircraft_list], dtype=dtype)
-        self.alt = np.array([aircraft.alt for aircraft in aircraft_list], dtype=dtype)
-        self.vs_fph = np.array([aircraft.vs_fph for aircraft in aircraft_list], dtype=dtype)
-        self.active = np.array([aircraft.active for aircraft in aircraft_list], dtype=bool)
+    callsign_map = None
+
+    def __init__(self, position, trk, gs, alt, vs, callsign, active):
+        self.n = self._check_dimensions_and_get_n(position, trk, gs, alt, vs, callsign, active)
+        self.position = position
+        self.trk = trk
+        self.gs = gs
+        self.alt = alt
+        self.vs = vs
+        self.callsign = callsign
+        self.active = active
+        self.callsign_map = {}
+        self._populate_callsign_map(self.callsign)
+
+        self.v = (gs * np.array([np.sin(np.radians(trk)), np.cos(np.radians(trk))])).T
+        self.vs_fph = self.vs * 60.
+
+        self.index = np.arange(self.n, dtype=int)
+        self.aircraft = [AircraftInFlow(i, self.position, self.trk, self.gs, self.alt, self.vs, self.callsign,
+                                        self.active) for i in self.index]
+
+        self.collisions = np.empty((self.n, self.n), dtype=bool)
+
+        self._calculate_active_aircraft_combinations()
+        self._update_collisions()
+
+    def _calculate_active_aircraft_combinations(self):
+        self.active_aircraft_combinations = list(itertools.combinations_with_replacement(self.index[self.active], 2))
 
     def activate(self, callsign, deactivate=False):
         if deactivate:
@@ -89,6 +139,8 @@ class Flow:
             if self.active[self.callsign_map[callsign]]:
                 logging.warning("Aircraft {} was already active".format(callsign))
             self.active[self.callsign_map[callsign]] = True
+        self._calculate_active_aircraft_combinations()
+        self._update_collisions()
 
     def deactivate(self, callsign):
         return self.activate(callsign, True)
@@ -96,6 +148,28 @@ class Flow:
     def step(self, dt):
         self.position[self.active] += self.v[self.active] * dt
         self.alt[self.active] += self.vs_fph[self.active] * dt
+        self._update_collisions()
+
+    def _update_collisions(self):
+        # Inactive aircraft have no collisions
+        self.collisions[:, :] = False
+        for i, j in self.active_aircraft_combinations:
+            if i != j:
+                self.collisions[i, j] = self.collisions[j, i] = self.aircraft[i] & self.aircraft[j]
+
+    def _populate_callsign_map(self, callsign_list):
+        for i, callsign in enumerate(callsign_list):
+            if callsign in self.callsign_map.keys():
+                raise ValueError("Duplicate callsign detected")
+            self.callsign_map[callsign] = i
+
+    @staticmethod
+    def _check_dimensions_and_get_n(*args):
+        n = args[0].shape[0]
+        for arg in args:
+            if arg.shape[0] != n:
+                raise ValueError('Not all arguments have same first dimension')
+        return n
 
 
 if __name__ == "__main__":
--- tests/test_conflicts_simulate.py
+++ tests/test_conflicts_simulate.py
@@ -2,30 +2,42 @@ import logging
 
 import numpy as np
 import pytest
+from pint import UnitRegistry
 from pytest import approx
-from pint import UnitRegistry, Quantity
 
-from conflicts.simulate import Aircraft, Flow, get_in_unit
+from conflicts.simulate import Aircraft, SingleAircraft, Flow, AircraftInFlow
+
 ureg = UnitRegistry()
 
-def test_Aircraft():
-    ac1 = Aircraft((0, 0), 90, 100, 0, 0, 'ac1')
+
+def test_aircraft():
+    ac = Aircraft((0, 0), 90, 100, 2000, -100, 'ac', True)
+    with pytest.raises(NotImplementedError):
+        ac.trk = 0
+    with pytest.raises(NotImplementedError):
+        ac.gs = 0
+    with pytest.raises(NotImplementedError):
+        ac.vs = 0
+
+
+def test_single_aircraft():
+    ac1 = SingleAircraft((0, 0), 90, 100, 0, 0, 'ac1')
     assert ac1.v.dtype == ac1.dtype
     assert ac1.position.shape == (2,)
     assert ac1.position.dtype == ac1.dtype
 
     ac1.step(1)
     assert np.allclose(ac1.position, [100, 0])
-    ac1_respawn = Aircraft((0, 0), 90, 100, 0, 0, 'ac1')
+    ac1_respawn = SingleAircraft((0, 0), 90, 100, 0, 0, 'ac1')
     for _ in range(3600):
-        ac1_respawn.step(1/3600.)
+        ac1_respawn.step(1 / 3600.)
     assert np.allclose(ac1_respawn.position, [100, 0])
 
-    ac2 = Aircraft((3, 4), np.degrees(0.5*np.pi - np.arctan2(4, 3)), 5, 0, 0, 'ac2')
+    ac2 = SingleAircraft((3, 4), np.degrees(0.5 * np.pi - np.arctan2(4, 3)), 5, 0, 0, 'ac2')
     ac2.step(1)
     assert np.allclose(ac2.position, [6, 8])
 
-    ac3 = Aircraft((0, 0), 0, 100, 6000, -100, 'ac3')
+    ac3 = SingleAircraft((0, 0), 0, 100, 6000, -100, 'ac3')
     assert ac3.vs_fph == approx(-6000)
     ac3.step(1)
     assert ac3.alt == approx(0)
@@ -40,24 +52,24 @@ def test_Aircraft():
     ac3.step(1)
     assert ac3.position == approx((150, 100))
 
-    two_hour = 2*3600*ureg.second
+    two_hour = 2 * 3600 * ureg.second
     ac3.step(two_hour)
     assert ac3.position == approx((250, 100))
 
     with pytest.raises(NotImplementedError):
         ac1 & 3
 
-    assert ac1 & ac1_respawn == True
-    assert ac1 & ac2 == False
+    assert ac1 & ac1_respawn
+    assert not ac1 & ac2
 
-    assert Aircraft((0, 0), 0, 100, 0, 0, 'ac1') & Aircraft((0, 3.), 0, 100, 0, 0, 'ac1')
-    assert Aircraft((0, 0), 0, 100, 0, 0, 'ac1') & Aircraft((0, 3.), 0, 100, 999, 0, 'ac1')
-    assert Aircraft((0, 0), 0, 100, 0, 0, 'ac1') & Aircraft((0, 3.), 0, 100, 1000., 0, 'ac1')
-    assert not Aircraft((0, 0), 0, 100, 0, 0, 'ac1') & Aircraft((0, 3.), 0, 100, 1001., 0, 'ac1')
-    assert not Aircraft((0, 0), 0, 100, 0, 0, 'ac1') & Aircraft((0, 3.1), 0, 100, 0., 0, 'ac1')
+    assert SingleAircraft((0, 0), 0, 100, 0, 0, 'ac1') & SingleAircraft((0, 3.), 0, 100, 0, 0, 'ac1')
+    assert SingleAircraft((0, 0), 0, 100, 0, 0, 'ac1') & SingleAircraft((0, 3.), 0, 100, 999, 0, 'ac1')
+    assert SingleAircraft((0, 0), 0, 100, 0, 0, 'ac1') & SingleAircraft((0, 3.), 0, 100, 1000., 0, 'ac1')
+    assert not SingleAircraft((0, 0), 0, 100, 0, 0, 'ac1') & SingleAircraft((0, 3.), 0, 100, 1001., 0, 'ac1')
+    assert not SingleAircraft((0, 0), 0, 100, 0, 0, 'ac1') & SingleAircraft((0, 3.1), 0, 100, 0., 0, 'ac1')
 
     x0 = (1., 2.)
-    inactive_ac = Aircraft(x0, 180, 100, 2000, 0, 'inactive', False)
+    inactive_ac = SingleAircraft(x0, 180, 100, 2000, 0, 'inactive', False)
     inactive_ac.step(0.1)
     assert inactive_ac.position == approx(x0)
 
@@ -65,28 +77,115 @@ def test_Aircraft():
 LOGGER = logging.getLogger(__name__)
 
 
-def test_Flow(caplog):
-    aircraft_list = [Aircraft((0, 0), 90, 100, 0, 0, 'ac1', True),
-                     Aircraft((0, 0), 360, 100, 0, 0, 'ac2', False),
-                     Aircraft((0, 0), 90, 100, 0, 0, 'ac3', False)]
-    flow = Flow(aircraft_list)
+@pytest.fixture
+def aircraft_in_flow_list():
+    pos = np.array([[0, 0], [1, 2], [10, 20]], dtype=Aircraft.dtype)
+    trk = np.array([0, 90, 270], dtype=Aircraft.dtype)
+    gs = np.array([100, 50, 150], dtype=Aircraft.dtype)
+    alt = np.array([0, 2000, 3000], dtype=Aircraft.dtype)
+    vs = np.array([100, 0, -100], dtype=Aircraft.dtype)
+    callsign = np.array(['ac1', 'ac2', 'ac3'])
+    active = np.array([True, True, False], dtype=bool)
+    index = np.arange(3, dtype=int)
+    ac = [AircraftInFlow(i, pos, trk, gs, alt, vs, callsign, active) for i in index]
+    return pos, trk, gs, alt, vs, callsign, active, index, ac
+
+
+def test_aircraft_in_flow(aircraft_in_flow_list):
+    pos, trk, gs, alt, vs, callsign, active, index, ac = aircraft_in_flow_list
+
+    with pytest.raises(RuntimeError):
+        ac[0].step(1)
+
+    for i in index:
+        assert ac[i].position == approx(pos[i:i + 1])
+        assert ac[i].trk == approx(trk[i])
+        assert ac[i].gs == approx(gs[i])
+        assert ac[i].alt == approx(alt[i])
+        assert ac[i].vs == approx(vs[i])
+        assert ac[i].callsign == callsign[i]
+        assert ac[i].active == active[i]
+
+
+def test_flow(caplog, aircraft_in_flow_list):
+    pos, trk, gs, alt, vs, callsign, active, index, ac = aircraft_in_flow_list
+
+    flow = Flow(pos, trk, gs, alt, vs, callsign, active)
     assert flow.position.shape == (3, 2)
     assert flow.v.shape == (3, 2)
-    assert flow.alt.shape == (3, )
-    assert flow.vs_fph.shape == (3, )
+    assert flow.alt.shape == (3,)
+    assert flow.vs_fph.shape == (3,)
     assert flow.active.shape == (3,)
 
-    assert flow.active == approx(np.array([True, False, False]))
+    assert flow.active == approx(np.array([True, True, False]))
+
     flow.step(1)
-    assert flow.position == approx(np.array([[100, 0], [0, 0], [0, 0]]))
+    v_expected = [[0, 100], [50, 0], [-150, 0]]
+    pos_expected = [[0, 100], [51, 2], [10, 20]]
+    alt_expected = [6000, 2000, 3000]
+    for i in index:
+        assert flow.v[i] == approx(v_expected[i])
+        assert flow.alt[i] == approx(alt_expected[i])
+        assert flow.position[i:i + 1] == approx(np.atleast_2d(pos_expected[i]))
+
+        assert ac[i].position == approx(pos[i:i + 1])
+        assert ac[i].trk == approx(trk[i])
+        assert ac[i].gs == approx(gs[i])
+        assert ac[i].alt == approx(alt[i])
+        assert ac[i].vs == approx(vs[i])
+        assert ac[i].callsign == callsign[i]
+        assert ac[i].active == active[i]
 
     flow.activate('ac1')
     assert 'ac1 was already active' in caplog.text
     flow.activate('ac1', deactivate=True)
 
-    flow.deactivate('ac2')
-    assert 'ac2 was already inactive' in caplog.text
-    flow.activate('ac2')
+    flow.deactivate('ac3')
+    assert 'ac3 was already inactive' in caplog.text
+    flow.activate('ac3')
 
     flow.step(1)
-    assert flow.position == approx(np.array([[100, 0], [0, 100], [0, 0]]))
+    assert flow.position == approx(np.array([[0, 100], [101, 2], [-140, 20]]))
+
+
+def test_flow_no_duplicate_callsigns(aircraft_in_flow_list):
+    pos, trk, gs, alt, vs, callsign, active, index, ac = aircraft_in_flow_list
+    with pytest.raises(ValueError):
+        callsign = np.array(['ac1', 'ac2', 'ac2'])
+        Flow(pos, trk, gs, alt, vs, callsign, active)
+
+
+@pytest.fixture
+def aircraft_on_collision():
+    pos = np.array([[0, 0], [10, 0], [5, 10], [5, 5]], dtype=Aircraft.dtype)
+    trk = np.array([90, 270, 180, 0], dtype=Aircraft.dtype)
+    gs = np.array([10, 10, 10, 5], dtype=Aircraft.dtype)
+    alt = np.array([2000, 1000, 2000, 2000], dtype=Aircraft.dtype)
+    vs = np.array([0, 1900 / 30., 0, 0], dtype=Aircraft.dtype)
+    callsign = np.array(['ac1', 'ac2', 'ac3', 'ac4'])
+    active = np.array([True, True, True, True], dtype=bool)
+    index = np.arange(4, dtype=int)
+    ac = [AircraftInFlow(i, pos, trk, gs, alt, vs, callsign, active) for i in index]
+    return pos, trk, gs, alt, vs, callsign, active, index, ac
+
+
+def test_flow_collision(aircraft_on_collision):
+    import copy
+
+    pos, trk, gs, alt, vs, callsign, active, index, ac = copy.deepcopy(aircraft_on_collision)
+    flow = Flow(pos, trk, gs, alt, vs, callsign, active)
+    assert ~np.any(flow.collisions)
+    flow.step(1.9 / 15)
+    assert ~np.any(flow.collisions)
+    flow.step(0.2 / 15)
+    assert flow.collisions[2, 3] and flow.collisions[3, 2]
+    flow.deactivate('ac4')
+    assert ~np.any(flow.collisions)
+    pos, trk, gs, alt, vs, callsign, active, index, ac = copy.deepcopy(aircraft_on_collision)
+    active[3] = False
+    flow = Flow(pos, trk, gs, alt, vs, callsign, active)
+    flow.step(0.5)
+    assert flow.collisions[0, 1] and flow.collisions[1, 0]
+    assert np.sum(flow.collisions) == 2
+    flow.step(0.1)
+    assert np.sum(flow.collisions) == 0
