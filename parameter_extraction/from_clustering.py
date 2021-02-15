4fdc19f9c8987ac9cd087e50eb9e32e59907a080
salomonv <salomon@salomonv.nl>
2021-02-15 16:25:01 +0100

    Parameter extraction: - Find intersection heatmap of mean tracks.

--- parameter_extraction/from_clustering.py
+++ parameter_extraction/from_clustering.py
@@ -2,6 +2,8 @@ import ast
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import geopandas
+from shapely.geometry import Point, LineString, shape
 
 
 # From https://github.com/daleroberts/hdmedians/blob/master/hdmedians/medoid.py
@@ -42,17 +44,34 @@ def medoid(a, axis=1, indexonly=False):
 
     raise IndexError("axis {} out of bounds".format(axis))
 
+
+def count_intersections(a, b):
+    if a == b:
+        return np.nan
+    if not a.intersects(b):
+        return 0
+    intersection = a.intersection(b)
+
+    if intersection.geom_type == 'Point':
+        return 1
+    elif intersection.geom_type == 'MultiPoint':
+        return len(intersection)
+    else:
+        raise NotImplementedError("Unknown geom_type {}".format(intersection.geom_type))
+
 if __name__ == "__main__":
     data_date = '20180101'
     n_data_points = 200
+    plot_intersection_heatmap = True
     with open('data/clustered/eham_{0}.csv'.format(data_date), 'r') as fp:
         parameters = ast.literal_eval(fp.readline())
         df = pd.read_csv(fp)
     df.sort_values(by=['cluster', 'ts'], inplace=True)
 
     tracks_per_cluster = df.groupby('cluster')['fid'].unique().apply(len)
-
-    for cluster, n_tracks in tracks_per_cluster.sort_values(ascending=False).iteritems():
+    rows = []
+    # for cluster, n_tracks in tracks_per_cluster.sort_values(ascending=False).iteritems():
+    for cluster, n_tracks in tracks_per_cluster.iteritems():
         # Don't use unclustered tracks for now
         if cluster == -1:
             continue
@@ -68,19 +87,32 @@ if __name__ == "__main__":
             assert np.all(cluster_points.loc[track_points.index[resampling_indices], 'index_along_track'] >= 0)
         points = cluster_points.query('index_along_track >= 0')
         points_per_fid = points.set_index(['fid', 'index_along_track']).sort_index()[['x', 'y']].to_numpy().reshape(n_tracks, 200, 2)
-        plt.figure()
-        medoid_index = medoid(points_per_fid.reshape(n_tracks, -1), axis=0, indexonly=True)
+        # plt.figure()
+        # medoid_index = medoid(points_per_fid.reshape(n_tracks, -1), axis=0, indexonly=True)
         mean = points_per_fid.reshape(n_tracks, -1).mean(axis=0).reshape(200, 2)
-        for i, trajectory in enumerate(points_per_fid):
-            if i == medoid_index:
-                plt.scatter(trajectory[:, 0], trajectory[:, 1], s=10, c='blue', label='Medoid')
-            else:
-                plt.scatter(trajectory[:, 0], trajectory[:, 1], s=0.1)
-        plt.scatter(mean[:, 0], mean[:, 1], s=10, c='orange', label='Mean')
-        plt.legend()
-        plt.show()
+        # for i, trajectory in enumerate(points_per_fid):
+        #     if i == medoid_index:
+        #         plt.scatter(trajectory[:, 0], trajectory[:, 1], s=10, c='blue', label='Medoid')
+        #     else:
+        #         plt.scatter(trajectory[:, 0], trajectory[:, 1], s=0.1)
+        # plt.scatter(mean[:, 0], mean[:, 1], s=10, c='orange', label='Mean')
+        # plt.legend()
+        # plt.show()
+
+        rows.append((cluster, n_tracks, LineString(mean)))
+
         # plt.figure()
         # points.plot.scatter(y='index_along_track', x='gs', s=0.01)
         # plt.show()
-        pass
+
+    gdf = geopandas.GeoDataFrame.from_records(rows, columns=['cluster', 'n_tracks', 'mean_track'])
+    intersection_map = np.array([[left.intersects(right) for left in gdf['mean_track']] for right in gdf['mean_track']])
+    intersection_heatmap = np.array([[count_intersections(left, right) for left in gdf['mean_track']] for right in gdf['mean_track']])
+
+    if plot_intersection_heatmap:
+        import searborn as sns
+        ax = sns.heatmap(intersection_heatmap / 200 * 100, cbar_kws={"label": "%"})
+        ax.set_title("% intra-cluster trajectory points intersecting per cluster pair")
+
+        plt.show()
 
